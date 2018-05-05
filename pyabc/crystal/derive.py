d8f2e4fcd09fa593bb660d397c9cb1473469e620
Jason.Yu <morty.yu@yahoo.com>
2018-05-05 15:33:35 +0800

    move hnf.py into derive.py

---
+++ pyabc/crystal/derive.py
@@ -0,0 +1,96 @@
+import numpy
+
+from math import sqrt
+from itertools import product
+
+from pyabc.crystal.structure import Cell, is_primitive_cell
+
+def _factor(n):
+    """
+    https://rosettacode.org/wiki/Factors_of_an_integer#Python
+
+    realize that factors come in pairs, the smaller of which is
+    no bigger than sqrt(n)
+    """
+    factors = set()
+    for x in range(1, int(sqrt(n)) + 1):
+        if n % x == 0:
+            factors.add(x)
+            factors.add(n // x)
+    return sorted(factors)
+
+
+def _hnfs(det):
+    h = []
+    for a in _factor(det):
+        for d in _factor(det // a):
+            f = det // a // d
+
+            for b, c, e in product(range(d), range(f), range(f)):
+                yield numpy.array([[a, b, c],
+                                   [0, d, e],
+                                   [0, 0, f]])
+
+
+def hnf_cells(pcell, volume=1, symprec=1e-5, comprec=1e-5):
+    """
+    hnf_cells return all non duplicated hnf extend cells.
+
+    parameters:
+
+    pcell: Cell object, The primitive cell to be extended
+    volume: int, Extend to how large supercellself, default=1
+    symprec: int, symmetry precision
+                When finding duplicated hnfs the precesion, default=5
+    comprec: float, compare precision
+                When finding the rotations symmetry of primitive cell, defalut=1e-5
+
+    return:
+
+    A list of Cell objects.
+    """
+    if not isinstance(pcell, Cell):
+        raise TypeError("Can't make hnf cells of {:} "
+                        "please provide pyabc.crystal.structure.Cell object.".format(type(pcell)))
+
+    if not is_primitive_cell(pcell):
+        raise ValueError("cell object you provide is not a primitive cell "
+                         "Therefore meaningless to get non duplicated hnf cells "
+                         "You can use pcell.get_primitive() first.")
+
+    nodup_hnfs = []
+    rot_list = pcell.get_rotations(symprec)
+    for hnf in _hnfs(volume):
+        if _not_contain(nodup_hnfs, hnf, rot_list, comprec):
+            nodup_hnfs.append(hnf)
+
+    nodup_cells = [pcell.extend(hnf) for hnf in nodup_hnfs]
+    return nodup_cells
+
+
+def _not_contain(hnf_list, hnf, rot_list, prec):
+    for h in hnf_list:
+        if _is_hnf_dup(hnf, h, rot_list, prec):
+            return False
+    return True
+
+
+def _is_hnf_dup(hnf_x, hnf_y, rot_list, prec=1e-5):
+    """
+    A hnf act in a cell,
+    if H_x * R.T^-1 * H_y ^-1 is an int matrix:
+        H_x and H_y produce the same supercell.
+
+    Algorithm: Hermite normal form (HNF) matrices remove translation symmetry duplications
+               However, rotation symmetry duplications also need to be removedself.
+               That is what this function ``_is_hnf_dup`` do.
+    """
+    # TODO: efficiency!
+    for rot in rot_list:
+        m = numpy.matmul(
+            numpy.matmul(hnf_x, numpy.linalg.inv(rot.T)),
+            numpy.linalg.inv(hnf_y))
+        if numpy.allclose(numpy.mod(m, 1), numpy.zeros_like(m), atol=prec):
+            return True
+
+    return False
