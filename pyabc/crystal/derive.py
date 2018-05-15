644e5b66bce7051ecb70acbd7e6932a22e9da219
Jason.Yu <morty.yu@yahoo.com>
2018-05-15 17:38:51 +0800

    genconf more beautiful

--- pyabc/crystal/derive.py
+++ pyabc/crystal/derive.py
@@ -28,7 +28,17 @@ def cells_nonredundant(pcell, volume=1, symprec=1e-5, comprec=1e-5):
         yield pcell.extend(hnf)
 
 
-def configurations_nonredundant(pcell, sites, volume=1, symprec=1e-5):
+# 在较大体积的构型中，可能有结构已经在小的体积中出现过。
+# 那么是否要移除这些已经出现过的结构呢？
+#
+# 暂时给出的答案是：若要目的是产生基态相图，则出现过的无需重复计算。
+#     若目的是计算结构能量做统计平均，则在每个体积倍数下需要得到简并度，以及总体数目，
+#     所以该体积倍数下的所有构型都是需要的，包括完全不取代的构型。
+#     TODO: 在以后的发展中，可以用到结构对比的技术，以允许统计的情况下无需重复计算。
+#
+# 该函数中没有comprec参数，是因为构型(configurations)之间的对比
+# 通过原子排列的对比实现，没有坐标的比较。
+def configurations_nonredundant(pcell, sites, max_volume=2, symprec=1e-5):
     """
     parameters:
 
@@ -39,49 +49,47 @@ def configurations_nonredundant(pcell, sites, volume=1, symprec=1e-5):
 
     Cell object, a list of non-redundant configurations
 
-    在较大体积的构型中，可能有结构已经在小的体积中出现过。
-
-    该函数中没有comprec参数，是因为构型(configurations)之间的对比
-    通过原子排列的对比实现，没有坐标的比较。
     """
-    hnfs = non_dup_hnfs(pcell, volume, symprec)
-    for h in hnfs:
-        hfpg = HFPG(pcell, h)
-        # TODO: 此处的平移操作不必每次重新计算，因为相同snf平移操作相同
-        # 可以用字典来标记查询。若没有这样的操作，那么就没有snf带来的效率提升。
-        # translations = hfpg.get_pure_translations(symprec)
-        supercell = pcell.extend(h)
-        # TODO: 加入一个机制，来清晰的设定位点上无序的状态
-        perm = hfpg.get_symmetry()
-        arg_sites_pcell = [len(i) for i in sites]
-        arg_sites = numpy.repeat(arg_sites_pcell, volume)
+    # 该函数产生所有构型用于确定基态相图
+    for v in range(1, max_volume + 1):
+        hnfs = non_dup_hnfs(pcell, v, symprec)
+        for h in hnfs:
+            hfpg = HFPG(pcell, h)
+            # TODO: 此处的平移操作不必每次重新计算，因为相同snf平移操作相同
+            # 可以用字典来标记查询。若没有这样的操作，那么就没有snf带来的效率提升。
+            translations = hfpg.get_pure_translations(symprec)
+            supercell = pcell.extend(h)
+            # TODO: 加入一个机制，来清晰的设定位点上无序的状态
+            perm = hfpg.get_symmetry()
+            arg_sites_pcell = [len(i) for i in sites]
+            arg_sites = numpy.repeat(arg_sites_pcell, v)
 
-        # actual results
-        res = []
-        # redundant configurations do not want see again
-        # 用于记录在操作作用后已经存在的构型排列，而无序每次都再次对每个结构作用所有操作
-        redundant = set()
+            # actual results
+            res = []
+            # redundant configurations do not want see again
+            # 用于记录在操作作用后已经存在的构型排列，而无序每次都再次对每个结构作用所有操作
+            redundant = set()
 
-        # loop over configurations
-        for atoms_mark in atoms_gen(arg_sites):
-            # import pdb; pdb.set_trace()
-            atoms_mark_arr = numpy.array(atoms_mark)
-            ahash = hash_atoms(atoms_mark)
-            if ahash in redundant:
-                continue
-            else:
-                res.append(atoms_mark)
+            # loop over configurations
+            for atoms_mark in atoms_gen(arg_sites):
+                # import pdb; pdb.set_trace()
+                atoms_mark_arr = numpy.array(atoms_mark)
+                flag_super = _is_super(atoms_mark_arr, translations)
+                ahash = hash_atoms(atoms_mark)
+                # import pdb; pdb.set_trace()
+                if (ahash in redundant) or flag_super:
+                    continue
+                else:
+                    for p in perm:
+                        atoms_transmuted = atoms_mark_arr[p]
+                        redundant.add(hash_atoms(atoms_transmuted))
+                    print("{:}".format(h.flatten()) +
+                          '  ' + hash_atoms(atoms_mark))
 
-            # for diff hnf loop over rots and trans, aka transfromations
-            for p in perm:
-                atoms_transmuted = atoms_mark_arr[p]
-                redundant.add(hash_atoms(atoms_transmuted))
 
-        print("hnf is:")
-        print(h)
-        print(supercell.lattice)
-        print(supercell.positions)
-        for atoms in res:
-            # yield Cell(supercell.lattice, supercell.positions, atoms)
-            print(atoms)
-        print('\n')
+def _is_super(atoms_mark_arr, translations):
+    for t in translations[1:]:
+        transmuted = atoms_mark_arr[t]
+        if numpy.array_equal(transmuted, atoms_mark_arr):
+            return True
+    return False
--- pyabc/crystal/tests/test_derive.py
+++ pyabc/crystal/tests/test_derive.py
@@ -7,20 +7,20 @@ from pyabc.crystal.derive import configurations_nonredundant
 class TestDerive(unittest.TestCase):
 
     def test_conf_non_redun(self):
-        # fcc_latt = [0, 5, 5,
-        #             5, 0, 5,
-        #             5, 5, 0]
-        # fcc_pos = [(0, 0, 0)]
-        # fcc_atoms = [0]
-        # fcc_pcell = Cell(fcc_latt, fcc_pos, fcc_atoms)
-        # configurations_nonredundant(fcc_pcell, [[3, 5]], 2)
-        sc_latt = [5, 0, 0,
-                   0, 5, 0,
-                   0, 0, 5]
-        sc_pos = [(0, 0, 0)]
-        sc_atoms = [0]
-        sc_pcell = Cell(sc_latt, sc_pos, sc_atoms)
-        configurations_nonredundant(sc_pcell, [[3, 5]], 4)
+        fcc_latt = [0, 5, 5,
+                    5, 0, 5,
+                    5, 5, 0]
+        fcc_pos = [(0, 0, 0)]
+        fcc_atoms = [0]
+        fcc_pcell = Cell(fcc_latt, fcc_pos, fcc_atoms)
+        configurations_nonredundant(fcc_pcell, [[3, 5]], 8)
+        # sc_latt = [5, 0, 0,
+        #            0, 5, 0,
+        #            0, 0, 5]
+        # sc_pos = [(0, 0, 0)]
+        # sc_atoms = [0]
+        # sc_pcell = Cell(sc_latt, sc_pos, sc_atoms)
+        # configurations_nonredundant(sc_pcell, [[3, 5]], 4)
 
 
 if __name__ == "__main__":
