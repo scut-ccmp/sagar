1efc7ae7cc5a6d1f5bb2f87fe9727592650c8788
Jason.Yu <morty.yu@yahoo.com>
2019-04-22 15:20:14 +0800

    release.sh and new setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,42 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+__author__ = "Jason Eu"
+__license__ = "MIT"
+
+about = {}
+with open('sagar/version.py') as f:
+    exec(f.read(), about)
+
 setup(
-    name='sagar',
+    name = 'sagar',
     # Remenber change version in ababe.cmdline.runabalib.exec_from_cmdline()
-    version='0.1.0',
-    description='A grid structure machine learning tool used\
-                 in material science.',
-    author='Jason Yu',
+    version = about['__version__'],
+    description='Structures of Alloys Generation And Recognition',
+    long_description=open('README.rst').read()
+    author='Jason Eu',
     author_email='morty.yu@yahoo.com',
-    license='MIT',
-    packages=find_packages(),
+    license=__license__,
+    packages=['sagar'],
     include_package_data=True,
     package_data={},
-    install_requires=["nose2", "numpy", "spglib==1.9.9.18",
-                      "click>=6", "colorama", "ase==3.16"],
+    keywords = 'crystal material strucutre DFT'
+    install_requires=[
+        "numpy",
+        "spglib",
+        "click>=6",
+        "colorama",
+        "ase==3.16",
+    ],
+    extras_require={
+        'dev': [
+            'pip',
+            'pytest',
+            'pytest-cov',
+            'twine',
+            'ipython',
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.5",
@@ -25,7 +48,6 @@ setup(
         "Topic :: Scientific/Engineering :: Physics",
         "Natural Language :: Chinese (Simplified)",
         "Natural Language :: English"],
-    scripts=[],
     entry_points={
         'console_scripts': [
             'rexpand = sagar.cmd.rexpand:cli',
